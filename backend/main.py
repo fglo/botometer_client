2bde3566b06d1cfb6dc203458622cfca8657b345
fglo <fmglowacki@gmail.com>
2021-04-19 13:00:13 +0200

    added configuration for visual studio code debugging

--- backend/main.py
+++ backend/main.py
@@ -50,7 +50,8 @@ def create_app():
         pass
     
     return app
-    
+
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     uvicorn.run(app, host="0.0.0.0", port=8000)
